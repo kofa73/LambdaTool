938e484101bfe14319b261034575f68db22ce230
Kofa <kofa@kovacs-telekes.org>
2021-11-25 18:47:32 +0100

    Adde factory method for the ByteBuddy instance

--- src/main/java/org/kovacstelekes/lambdatool/BbLambdaTool.java
+++ src/main/java/org/kovacstelekes/lambdatool/BbLambdaTool.java
@@ -4,6 +4,8 @@ import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.implementation.InvocationHandlerAdapter;
 import net.bytebuddy.matcher.ElementMatchers;
 
+import java.lang.reflect.InvocationHandler;
+
 public class BbLambdaTool<T> extends AbstractLambdaTool<T> {
     private BbLambdaTool(T buddy, CapturingInvocationHandler invocationHandler) {
         super(buddy, invocationHandler);
@@ -11,6 +13,11 @@ public class BbLambdaTool<T> extends AbstractLambdaTool<T> {
 
     public static <T> BbLambdaTool<T> forType(Class<T> type) {
         CapturingInvocationHandler invocationHandler = new CapturingInvocationHandler();
+        T buddy = createBuddy(type, invocationHandler);
+        return new BbLambdaTool<>(buddy, invocationHandler);
+    }
+
+    private static <T> T createBuddy(Class<T> type, InvocationHandler invocationHandler) {
         T buddy;
         try {
             buddy = new ByteBuddy()
@@ -25,6 +32,6 @@ public class BbLambdaTool<T> extends AbstractLambdaTool<T> {
         } catch (ReflectiveOperationException e) {
             throw new RuntimeException(e);
         }
-        return new BbLambdaTool<>(buddy, invocationHandler);
+        return buddy;
     }
 }
